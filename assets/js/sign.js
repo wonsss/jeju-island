7aba4a23d2c8891cdd93d0a32d1e58bf89383f89
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2023-12-14 19:32:14 +0900

    리팩토링 - errorDisplay, clearDisplay 함수 추가

--- assets/js/sign.js
+++ assets/js/sign.js
@@ -3,13 +3,23 @@ const input_pw = document.querySelector('#password');
 const input_pw_chk = document.querySelector('#password_chk');
 const error_msgs = document.querySelectorAll('.error_msg');
 const sign_form = document.querySelector('#sign_form');
+const currentPage = window.location.pathname;
 
 input_email.addEventListener('focusout', validateEmail);
 input_pw.addEventListener('focusout', validatePassword);
-input_pw_chk.addEventListener('focusout', checkPasswordMatch);
-sign_form.addEventListener('submit', validateLogin);
 
-function validateLogin(e) {
+// 로그인 페이지일 때
+if (currentPage === '/signin.html') {
+  sign_form.addEventListener('submit', validateLogin);
+}
+
+// 회원가입 페이지일 때
+else if (currentPage === '/signup.html') {
+  input_pw_chk.addEventListener('focusout', checkPasswordMatch);
+  sign_form.addEventListener('submit', validateJoin);
+}
+
+function validateLogin() {
   e.preventDefault();
   const emailValid = validateEmail();
   const passwordValid = validatePassword();
@@ -21,23 +31,33 @@ function validateLogin(e) {
   }
 }
 
-function validateEmail() {
+function validateJoin(e) {
+  e.preventDefault();
+  const emailValid = validateEmail();
+  const passwordValid = validatePassword();
+  const matchPassword = checkPasswordMatch();
+
+  if (emailValid && passwordValid && matchPassword) {
+    sign_form.action = '/folder';
+    sign_form.method = 'post';
+    sign_form.submit();
+  }
+}
+
+function validateEmail(e) {
   const regex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
   const errorMsg = error_msgs[0];
   const email = input_email.value;
 
   if (email.length === 0) {
-    errorMsg.innerHTML = '이메일을 입력해주세요';
-    input_email.classList.add('input-error');
+    errorDisplay(e, errorMsg, '이메일을 입력해주세요');
     return false;
   } else if (!regex.test(email)) {
-    errorMsg.innerHTML = '올바른 이메일 주소가 아닙니다.';
-    input_email.classList.add('input-error');
+    errorDisplay(e, errorMsg, '올바른 이메일 주소가 아닙니다.');
     return false;
   } else if (email == 'test@codeit.com') {
-    errorMsg.innerHTML = '이미 사용 중인 이메일입니다.';
-    input_email.classList.add('input-error');
+    errorDisplay(e, errorMsg, '이미 사용 중인 이메일입니다.');
     return false;
   } else {
     errorMsg.innerHTML = '';
@@ -46,36 +66,33 @@ function validateEmail() {
   }
 }
 
-function validatePassword() {
+function validatePassword(e) {
   const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
   const errorMsg = error_msgs[1];
   const password = input_pw.value;
 
   if (password.length === 0) {
-    errorMsg.innerHTML = '비밀번호를 입력해주세요.';
-    input_pw.classList.add('input-error');
+    errorDisplay(e, errorMsg, '비밀번호를 입력해주세요.');
     return false;
   } else if (!regex.test(password)) {
-    errorMsg.innerHTML = '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.';
-    input_pw.classList.add('input-error');
+    errorDisplay(e, errorMsg, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
+    return false;
   } else {
-    errorMsg.innerHTML = '';
-    input_pw.classList.remove('input-error');
+    clearDisplay(e, errorMsg);
     return true;
   }
 }
 
-function checkPasswordMatch() {
+function checkPasswordMatch(e) {
   const errorMsg = error_msgs[2];
   const password = input_pw.value;
   const passwordchk = input_pw_chk.value;
 
   if (passwordchk !== password) {
-    errorMsg.innerHTML = '비밀번호가 일치하지 않아요.';
-    input_pw_chk.classList.add('input-error');
+    errorDisplay(e, errorMsg, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
+    return false;
   } else {
-    errorMsg.innerHTML = '';
-    input_pw_chk.classList.remove('input-error');
+    clearDisplay(e, errorMsg);
     return true;
   }
 }
@@ -90,4 +107,13 @@ function verifyUserLogin() {
     return false;
   }
   return true;
+}
+
+function errorDisplay(e, errorMsg, message) {
+  errorMsg.innerHTML = message;
+  e.target.classList.add('input-error');
+}
+function clearDisplay(e, errorMsg) {
+  errorMsg.innerHTML = '';
+  e.target.classList.remove('input-error');
 }
